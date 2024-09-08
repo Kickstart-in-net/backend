4e7915140c31b4cf6157dd76cf583381e754d103
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2024-09-09 00:16:07 +0530

    Refactor PageResponse class and add annotations to UserController, UserMapper, UserRequest, UserResponse, and UserService classes

--- src/main/java/com/kickstart/backend/requests/UserRequest.java
+++ src/main/java/com/kickstart/backend/requests/UserRequest.java
@@ -1,4 +1,22 @@
 package com.kickstart.backend.requests;
 
-public class UserRequest {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public record UserRequest(
+        @NotNull
+        @Email
+        String email,
+        @NotNull @NotEmpty
+        String fullName,
+        @NotNull @NotEmpty
+        String bio,
+        @NotNull @NotEmpty
+        String profileUrl,
+        @NotNull @NotEmpty
+        String phone
+) {
+
+
 }
--- src/main/java/com/kickstart/backend/responses/UserResponse.java
+++ src/main/java/com/kickstart/backend/responses/UserResponse.java
@@ -1,4 +1,19 @@
 package com.kickstart.backend.responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserResponse {
+    private Long id;
+    private String fullName;
+    private String bio;
+    private String email;
+    private String phone;
+    private String profileUrl;
 }
