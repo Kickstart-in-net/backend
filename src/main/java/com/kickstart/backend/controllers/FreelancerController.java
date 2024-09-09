7ede388814cc82c250dbb5b266634517985e26d6
prabuddha369 <prabuddhapiku@gmail.com>
2024-09-09 18:41:48 +0530

    Freelancer Service added

--- src/main/java/com/kickstart/backend/controllers/FreelancerController.java
+++ src/main/java/com/kickstart/backend/controllers/FreelancerController.java
@@ -2,8 +2,11 @@ package com.kickstart.backend.controllers;
 
 import com.kickstart.backend.entities.Freelancer;
 import com.kickstart.backend.services.FreelancerService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class FreelancerController {
@@ -14,22 +17,37 @@ public class FreelancerController {
     }
 
     @GetMapping("users/freelancer/{id}")
-    public ResponseEntity<Freelancer> getFreelancer(@PathVariable Long id){
-        return freelancerService.getFreelancerById(id);
+    public ResponseEntity<Object> getFreelancer(@PathVariable Long id){
+        Map<String,Object> response = freelancerService.getFreelancerById(id);
+        return getObjectResponseEntity(response);
     }
 
     @PostMapping("users/freelancer/register")
-    public ResponseEntity<String> postFreelancer(@RequestBody Freelancer freelancer) {
-        return freelancerService.postFreelancer(freelancer);
+    public ResponseEntity<Object> postFreelancer(@RequestBody Freelancer freelancer) {
+        Map<String,Object> response = freelancerService.postFreelancer(freelancer);
+        return getObjectResponseEntity(response);
     }
 
     @PutMapping("users/freelancer/{id}")
-    public ResponseEntity<String> updateFreelancer(@PathVariable Long id, @RequestBody Freelancer freelancer) {
-        return freelancerService.putFreelancer(id, freelancer);
+    public ResponseEntity<Object> updateFreelancer(@PathVariable Long id, @RequestBody Freelancer freelancer) {
+        Map<String,Object> response = freelancerService.putFreelancer(id,freelancer);
+        return getObjectResponseEntity(response);
     }
 
     @DeleteMapping("users/freelancer/{id}")
-    public ResponseEntity<String> deleteFreelancer(@PathVariable Long id){
-        return freelancerService.deleteFreelancer(id);
+    public ResponseEntity<Object> deleteFreelancer(@PathVariable Long id){
+        Map<String,Object> response = freelancerService.deleteFreelancer(id);
+        return getObjectResponseEntity(response);
+    }
+
+    private ResponseEntity<Object> getObjectResponseEntity(Map<String, Object> response) {
+        if(response.get("status").equals(200))
+            return new ResponseEntity<>(response.get("object"), HttpStatus.OK);
+        else if(response.get("status").equals(201))
+            return new ResponseEntity<>(response.get("object"),HttpStatus.CREATED);
+        else if(response.get("status").equals(400))
+            return new ResponseEntity<>(response.get("object"),HttpStatus.BAD_REQUEST);
+        else
+            return new ResponseEntity<>(response.get("object"),HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- src/main/java/com/kickstart/backend/services/FreelancerService.java
+++ src/main/java/com/kickstart/backend/services/FreelancerService.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class FreelancerService {
     private final FreelancerRepository freelancerRepository;
@@ -15,55 +18,87 @@ public class FreelancerService {
         this.freelancerRepository = freelancerRepository;
     }
 
-    public ResponseEntity<Freelancer> getFreelancerById(Long id) {
+    public Map<String,Object> getFreelancerById(Long id) {
+        Map<String, Object> response = new HashMap<>();
         try {
             Freelancer freelancerData = freelancerRepository.findById(id).orElse(null);
-            return new ResponseEntity<>(freelancerData, HttpStatus.OK);
+            response.put("object", freelancerData);
+            response.put("status",200);
+            return response;
         } catch (DataIntegrityViolationException e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            response.put("object", "{\"message\":\"Data integrity violation occurred:"+ e.getMessage()+"\"}");
+            response.put("status",400);
+            return response;
         } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            response.put("object", "{\"message\":\"Internal Server error: "+ e.getMessage()+"\"}");
+            response.put("status",500);
+            return response;
         }
     }
 
-    public ResponseEntity<String> postFreelancer(Freelancer freelancer) {
+    public Map<String,Object> postFreelancer(Freelancer freelancer) {
+        Map<String, Object> response = new HashMap<>();
         try {
             freelancerRepository.save(freelancer);
-            return new ResponseEntity<>("Data added successfully", HttpStatus.CREATED);
+            response.put("object", "{\"message\":\"Data added successfully\"}");
+            response.put("status",201);
+            return response;
         } catch (DataIntegrityViolationException e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            response.put("object", "{\"message\":\"Data integrity violation occurred:"+ e.getMessage()+"\"}");
+            response.put("status",400);
+            return response;
         } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            response.put("object", "{\"message\":\"Internal Server error: "+ e.getMessage()+"\"}");
+            response.put("status",500);
+            return response;
         }
     }
 
-    public ResponseEntity<String> putFreelancer(Long id, Freelancer freelancer) {
+    public Map<String,Object> putFreelancer(Long id, Freelancer freelancer) {
+        Map<String, Object> response = new HashMap<>();
         try {
             if (!freelancerRepository.existsById(id)) {
-                return new ResponseEntity<>("Freelancer not found", HttpStatus.NOT_FOUND);
+                response.put("object","{\"message\":\"Data Not Found\"}");
+                response.put("status",404);
+                return response;
             }
             // Set the id of the freelancer object to the one being updated
             freelancer.setId(id);
             freelancerRepository.save(freelancer);
-            return new ResponseEntity<>("Data updated successfully", HttpStatus.OK);
+            response.put("object","{\"message\":\"Data Updated Successfully\"}");
+            response.put("status",200);
+            return response;
         } catch (DataIntegrityViolationException e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            response.put("object", "{\"message\":\"Data integrity violation occurred:"+ e.getMessage()+"\"}");
+            response.put("status",400);
+            return response;
         } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            response.put("object", "{\"message\":\"Internal Server error: "+ e.getMessage()+"\"}");
+            response.put("status",500);
+            return response;
         }
     }
 
-    public ResponseEntity<String> deleteFreelancer(Long id) {
+    public Map<String,Object> deleteFreelancer(Long id) {
+        Map<String, Object> response = new HashMap<>();
         try {
             if (!freelancerRepository.existsById(id)) {
-                return new ResponseEntity<>("Freelancer not found", HttpStatus.NOT_FOUND);
+                response.put("object","{\"message\":\"Data Not Found\"}");
+                response.put("status",404);
+                return response;
             }
             freelancerRepository.deleteById(id);
-            return new ResponseEntity<>("Deleted Successfully", HttpStatus.OK);
+            response.put("object","{\"message\":\"Data Deleted Successfully\"}");
+            response.put("status",200);
+            return response;
         } catch (DataIntegrityViolationException e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            response.put("object", "{\"message\":\"Data integrity violation occurred:"+ e.getMessage()+"\"}");
+            response.put("status",400);
+            return response;
         } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            response.put("object", "{\"message\":\"Internal Server error: "+ e.getMessage()+"\"}");
+            response.put("status",500);
+            return response;
         }
     }
 }
